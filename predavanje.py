93761234557dc1da6decf32d292390f3417e7abe
Ervina3 <163838584+Ervina3@users.noreply.github.com>
2024-04-01 09:24:51 +0000

    dodat fajl predavanje.py

--- predavanje.py
+++ predavanje.py
@@ -39,6 +39,14 @@ plt.ylabel('CIjena akcija')
 plt.title('Kreitanje akcija')
 plt.savefig('akcije.png')
 '''
+# pie chart
+import csv
+f=open('pets.csv')
+sadrzaj= csv.DictReader(f)
+data= next(sadrzaj)
+print(data)
 
-
-
+pets= list(data.keys())
+num_pets=[row for row in data.values()]
+plt.pie(num_pets, labels=pets)
+plt.savefig('pie.png')
